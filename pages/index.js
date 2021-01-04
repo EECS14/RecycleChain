061d89b2edc2a2c85b93b2346570c88445617353
EECS14 <100044615@ku.ac.ae>
2021-01-04 14:19:14 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 
-export default() => {
-    return <h1> first page </h1>; 
-};
+class index extends Component{
+
+    static async getInitalProbs(){
+    // Code here runs on the server - used for dataloading before the html is loaded on the client 
+    // Is a class function which mean it gets called as ClassName.getInitalProbs 
+    // Have to return the contract instance 
+    
+    }
+    
+    render(){
+    
+        return (
+            <h1>Hellow world2</h1>
+        );
+    }
+    
+    }
+    
+    //At the end of each page, a component is expected to be returned 
+    // If not, Next.js throws an error 
+    export default index; 
