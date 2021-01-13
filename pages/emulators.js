ed5e94f11e1fae8e075e361a2d88d46232772c2b
EECS14 <100044615@ku.ac.ae>
2021-01-13 17:37:46 +0400

    Create emulators.js

---
+++ pages/emulators.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'; 
+
+class index extends Component{
+
+    
+    
+    render(){
+    
+        return (
+            <h1>Hellow world2</h1>
+        );
+    }
+    
+    }
+    
+    //At the end of each page, a component is expected to be returned 
+    // If not, Next.js throws an error 
+    export default index; 
