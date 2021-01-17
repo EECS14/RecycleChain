53f8f53708c3ce7e1585524ec8308e4ba477b97b
EECS14 <100044615@ku.ac.ae>
2021-01-17 22:24:27 +0400

    Update register.js

--- ethereum/register.js
+++ ethereum/register.js
@@ -1,5 +1,6 @@
 /*
 purpose: To access the contract (register.sol) instance from inside the application without repeating these steps 
+Etherscan: https://ropsten.etherscan.io/tx/0x1569a64e3d51a7f59a41e80773d63a3128b8e740dca7109ead72378257c0d4df
 */
 import web3 from './web3'; 
 import register from './build/Register.json'; 
