77fc0592d550cf5630ffa5602dc1d027c174e1fb
EECS14 <100044615@ku.ac.ae>
2021-01-17 22:22:43 +0400

    Update JSON ABI's
    - in tracking.json and plasticbale.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6b2888ac215ba1b00914";
+/******/ 	var hotCurrentHash = "353badce20275a1405d8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
