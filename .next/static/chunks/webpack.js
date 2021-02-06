10780764374e1b9d1bfa3dc6669dcfc9a8fcead8
EECS14 <100044615@ku.ac.ae>
2021-02-06 12:55:04 +0400

    Update show.js
    - Add a dynamic timeline

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6f583e43c69bcadfe218";
+/******/ 	var hotCurrentHash = "573ea9efe4201c7b4fc7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/track/show.js
+++ pages/track/show.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Icon, Step } from 'semantic-ui-react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import web3 from '../../ethereum/web3';
 import trackingContract from '../../ethereum/tracking';
 import registerContract from '../../ethereum/register';
@@ -13,7 +15,7 @@ class show extends Component {
         this.state = {
             sortingFacilityAddr: '',
             disposeDate: '',
-            recyclerAddr:'',
+            recyclerAddr: '',
             sortedActive: false,
             sortedDisabled: true,
             sortDate: '',
@@ -21,12 +23,12 @@ class show extends Component {
             sortingFacilityLoc: '',
             sortingFacilityAddr: '',
             producedBales: [],
-            purchaseDate: '', 
+            purchaseDate: '',
             buyerAddr: '',
-            buyerName: '', 
+            buyerName: '',
             buyerLocation: '',
-            buyerBusiness:'',
-            purchaseActive:false,
+            buyerBusiness: '',
+            purchaseActive: false,
             purchaseDisabled: true
         };
     }
@@ -55,7 +57,7 @@ class show extends Component {
             //console.log(event.returnValues['time']);
             //var date = time.toUTCString();
             var time = new Date(event.returnValues['time'] * 1000);
-            var date = time.toString(); 
+            var date = time.toString();
             this.setState({ disposeDate: date });
         }.bind(this))
             .on('error', console.error);
@@ -68,36 +70,34 @@ class show extends Component {
         }, function (error, event) {
             this.setState({ sellerAddress: event.returnValues['sellerAddress'] });
             var time = new Date(event.returnValues['time'] * 1000);
-            var date = time.toString(); 
-            this.setState({ sortDate: date});
+            var date = time.toString();
+            this.setState({ sortDate: date });
             this.FetchSellerDetails();
-            this.setState({ sortedActive: true, sortedDisabled:false});
+            this.setState({ sortedActive: true, sortedDisabled: false });
         }.bind(this))
             .on('error', console.error);
-
 
         //************************************************************ 
         // Step: Purchased 
         // Checking the event happens inside 
-        this.FetchDeployedBales(); 
-    
-
-
+        this.FetchDeployedBales();
 
     };
 
 
     componentWillUnmount() {
-        this.setState({sortingFacilityAddr: '',
-        disposeDate: '',
-        recyclerAddr:'',
-        buyerAddr: '',
-        sortedActive: false,
-        sortedDisabled: true,
-        sortDate: '',
-        sortingFacilityName: '',
-        sortingFacilityLoc: '',
-        sortingFacilityAddr: ''});
+        this.setState({
+            sortingFacilityAddr: '',
+            disposeDate: '',
+            recyclerAddr: '',
+            buyerAddr: '',
+            sortedActive: false,
+            sortedDisabled: true,
+            sortDate: '',
+            sortingFacilityName: '',
+            sortingFacilityLoc: '',
+            sortingFacilityAddr: ''
+        });
     }
 
     // Get seller detail
@@ -118,36 +118,36 @@ class show extends Component {
 
     FetchDeployedBales = async () => {
 
-    //1. Find produced bales 
-    this.state.producedBales = await trackingContract.methods.getDeployedBales().call();
-    console.log(this.state.producedBales);
+        //1. Find produced bales 
+        this.state.producedBales = await trackingContract.methods.getDeployedBales().call();
+        console.log(this.state.producedBales);
 
-    let count =0; 
-    console.log(this.state.producedBales.length);
-    //2. Loop through all produced bales
-    while(Array.isArray(this.state.producedBales) && count != this.state.producedBales.length ) { 
-    console.log("tehe");
-    const plasticBaleSC = plasticBaleContract(this.state.producedBales[count]); 
-    //3. Filter events in each plasticbaleSC that is related to the bottle 
-    plasticBaleSC.events.updateStatusBuyer({
-        filter: { plasticBottleAddress: this.props.address }, fromBlock: 0
-    }, function (error, event) {
-        console.log(event); 
-        count = 0;
-        //4. Fetch details 
-        this.setState({ buyerAddr: event.returnValues['buyer'] });
-        var time = new Date(event.returnValues['time'] * 1000);
-        var date = time.toString(); 
-        this.setState({ purchaseDate: date}); 
-        this.FetchBuyerDetails(); 
-        this.setState({purchaseActive: true, purchaseDisabled:false}); 
-        
-    }.bind(this))
-        .on('error', console.error);
+        let count = 0;
+        console.log(this.state.producedBales.length);
+        //2. Loop through all produced bales
+        while (Array.isArray(this.state.producedBales) && count != this.state.producedBales.length) {
+            console.log("tehe");
+            const plasticBaleSC = plasticBaleContract(this.state.producedBales[count]);
+            //3. Filter events in each plasticbaleSC that is related to the bottle 
+            plasticBaleSC.events.updateStatusBuyer({
+                filter: { plasticBottleAddress: this.props.address }, fromBlock: 0
+            }, function (error, event) {
+                console.log(event);
+                count = 0;
+                //4. Fetch details 
+                this.setState({ buyerAddr: event.returnValues['buyer'] });
+                var time = new Date(event.returnValues['time'] * 1000);
+                var date = time.toString();
+                this.setState({ purchaseDate: date });
+                this.FetchBuyerDetails();
+                this.setState({ purchaseActive: true, purchaseDisabled: false });
 
-        count++; 
+            }.bind(this))
+                .on('error', console.error);
 
-}
+            count++;
+
+        }
     };
 
 
@@ -176,52 +176,70 @@ class show extends Component {
                 <link rel="stylesheet"
                     href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css"
                 />
-            <div className='Tracking'>
-                
-                <h1>You are currently tracking {this.props.address}</h1>
-
-                <Step.Group stackable='tablet'>
-                    <Step active>
-                        <Icon name='trash alternate' />
-                        <Step.Content >
-                            <Step.Title>Disposed</Step.Title>
-                            <Step.Description> Recycler: {this.state.recyclerAddr} </Step.Description>
-                            <Step.Description> Date: {this.state.disposeDate} </Step.Description>
-                        </Step.Content>
-                    </Step>
-                    <Step active={this.state.sortedActive} disabled={this.state.sortedDisabled}>
-                        <Icon name='qrcode' />
-                        <Step.Content>
-                            <Step.Title>Sorted</Step.Title>
-                            {this.state.sortedActive === true ? (
-                                 <div>
-                                    <Step.Description>Sorting Facility: {this.state.sortingFacilityName}</Step.Description>
-                                    <Step.Description>Location: {this.state.sortingFacilityLoc}</Step.Description>
-                                    <Step.Description>Address: {this.state.sortingFacilityAddr}</Step.Description>
-                                    <Step.Description>Date: {this.state.sortDate}</Step.Description>
-                                    </div>
-                            ): null}
-                            
-                        </Step.Content>
-                    </Step>
-                    <Step active={this.state.purchaseActive} disabled={this.state.purchaseDisabled}>
-                        <Icon name='factory' />
-                        <Step.Content>
-                            <Step.Title>Purchased</Step.Title>
-                            {this.state.purchaseActive === true ? (
-                                 <div>
-                                    <Step.Description>Buyer: {this.state.buyerName}</Step.Description>
-                                    <Step.Description>Location: {this.state.buyerLocation}</Step.Description>
-                                    <Step.Description>Address: {this.state.buyerAddr}</Step.Description>
-                                    <Step.Description>Recycled to: {this.state.buyerBusiness}</Step.Description>
-                                    <Step.Description>Date: {this.state.purchaseDate}</Step.Description>
-                                    </div>
-                            ): null}
-                        
-                        </Step.Content>
-                    </Step>
-                </Step.Group>
+                <div className='Tracking'>
+                    <h1>You are currently tracking {this.props.address}</h1>
+                    <br/>
                 </div>
+
+
+                <VerticalTimeline>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        iconStyle={{ background: 'rgb(66, 195, 99)', color: '#fff' }}
+                        icon= {<svg width="22" height="22" viewBox="0 0 32 32">
+                       <path d="M10 2h-8v8h8v-8zM12 0v0 12h-12v-12h12zM4 4h4v4h-4zM30 2h-8v8h8v-8zM32 0v0 12h-12v-12h12zM24 4h4v4h-4zM10 22h-8v8h8v-8zM12 20v0 12h-12v-12h12zM4 24h4v4h-4zM14 0h2v2h-2zM16 2h2v2h-2zM14 4h2v2h-2zM16 6h2v2h-2zM14 8h2v2h-2zM16 10h2v2h-2zM14 12h2v2h-2zM14 16h2v2h-2zM16 18h2v2h-2zM14 20h2v2h-2zM16 22h2v2h-2zM14 24h2v2h-2zM16 26h2v2h-2zM14 28h2v2h-2zM16 30h2v2h-2zM30 16h2v2h-2zM2 16h2v2h-2zM4 14h2v2h-2zM0 14h2v2h-2zM8 14h2v2h-2zM10 16h2v2h-2zM12 14h2v2h-2zM18 16h2v2h-2zM20 14h2v2h-2zM22 16h2v2h-2zM24 14h2v2h-2zM26 16h2v2h-2zM28 14h2v2h-2zM30 20h2v2h-2zM18 20h2v2h-2zM20 18h2v2h-2zM22 20h2v2h-2zM26 20h2v2h-2zM28 18h2v2h-2zM30 24h2v2h-2zM18 24h2v2h-2zM20 22h2v2h-2zM24 22h2v2h-2zM26 24h2v2h-2zM28 22h2v2h-2zM30 28h2v2h-2zM20 26h2v2h-2zM22 28h2v2h-2zM24 26h2v2h-2zM26 28h2v2h-2zM20 30h2v2h-2zM24 30h2v2h-2zM28 30h2v2h-2z"></path>
+
+                      </svg>}
+
+                    >
+                        <h3 className="vertical-timeline-element-title">Disposed</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Recycler: {this.state.recyclerAddr}</h4>
+                        <p>
+                        Date: {this.state.disposeDate}
+    </p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        iconStyle={{ background: 'rgb(66, 195, 99)', color: '#fff' }}
+                        icon={<svg width="22" height="22" viewBox="0 0 20 20">
+                        <path d="M10.5 20h-10.5v-13l5 3.33v-3.33l5 3.33v-3.33l5 3.33v-10.33h5v20h-9.5z"></path>
+                        </svg>}
+                        visible={this.state.sortedActive }
+
+                    >
+                        <h3 className="vertical-timeline-element-title">Sorted</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Address: {this.state.sortingFacilityAddr}</h4>
+                        <p>
+                        Sorting Facility: {this.state.sortingFacilityName} <br/>
+                        Location: {this.state.sortingFacilityLoc} <br/>
+                        Date: {this.state.sortDate}
+    </p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        iconStyle={{ background: 'rgb(66, 195, 99)', color: '#fff' }}
+                        icon={<svg width="22" height="22" viewBox="0 0 32 32 ">
+                        <path d="M15.925 23.96l-9.819-5.8 9.817 13.84 9.827-13.84-9.829 5.8zM16.075 0l-9.821 16.297 9.82 5.805 9.82-5.8z"></path>
+                        </svg>}
+                        visible={ this.state.purchaseActive }
+
+                    >
+                        <h3 className="vertical-timeline-element-title">Purchased</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Address: {this.state.buyerAddr}</h4>
+                        <p>
+                        Buyer: {this.state.buyerName} <br/>
+                        Location: {this.state.buyerLocation} <br/>
+                        Recycled to: {this.state.buyerBusiness} <br/>
+                        Date: {this.state.purchaseDate}
+    </p>
+                    </VerticalTimelineElement>
+        
+                    </VerticalTimeline>
+
+
+  
             </Layout>
 
         );
