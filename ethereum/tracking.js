245b1bd95e4b39a665adec549f33cc0c6a913180
EECS14 <100044615@ku.ac.ae>
2021-03-30 14:36:36 +0400

    Update tracking.js
    Redeploy tracking smart contract

--- ethereum/tracking.js
+++ ethereum/tracking.js
@@ -1,13 +1,13 @@
 /*
 purpose: To access the contract (register.sol) instance from inside the application without repeating these steps 
-Etherscan: https://ropsten.etherscan.io/tx/0x5121c0253fdfe09556bba5ce7ad7d0851a70a458fa8247a3790042865d212ff8
+Etherscan: https://ropsten.etherscan.io/tx/0x61ddd39f25c8f4e11e5353406c968e657d11530b9ef06f0466ed937316ad455e
 */
 import web3 from './web3'; 
 import Tracking from './build/Tracking.json'; 
 
 const instance = new web3.eth.Contract(
     Tracking.abi,
-     '0x9cdC88b5d9D5516669127D1b1937D59CDBAd5A21'
+     '0xBb13CdC1Ae2635699B98a3999C3509514F541641'
      // Address of the tracking.sol contract in ropsten network
 );
 
