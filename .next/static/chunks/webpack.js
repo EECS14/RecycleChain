f695a2033fc422da2585a4fa2051bb80ac56ef97
EECS14 <100044615@ku.ac.ae>
2021-03-31 12:37:19 +0400

    Install IPFS NPM depenecy

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "97882bf1fe76e63af902";
+/******/ 	var hotCurrentHash = "a860247d40aeab4a9caa";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
