024c10a1444ea433d88e332da49646bdf1fc8880
EECS14 <100044615@ku.ac.ae>
2021-04-13 01:17:09 +0400

    Update recycler.js
    Add selection menu in recycler page

--- pages/recycler.js
+++ pages/recycler.js
@@ -5,7 +5,7 @@ To run the app, use the command npm run dev
 */
 
 import React, { Component } from 'react';
-import { Card, Table, Button, Grid, Container } from 'semantic-ui-react';
+import { Card, Table, Button, Grid, Container, Menu } from 'semantic-ui-react';
 import dynamic from 'next/dynamic';
 const QRReader = dynamic(() => import('react-qr-reader'), { ssr: false });
 import web3 from '../ethereum/web3';
@@ -24,7 +24,8 @@ class recyclerPage extends Component {
             status: '',
             qr: false,
             rows: [],
-            bottlesLogged: []
+            bottlesLogged: [],
+            activeItem: 'scan'
         };
     }
 
@@ -36,7 +37,7 @@ class recyclerPage extends Component {
         trackingContract.events.updateStatusRecycler({
             filter: { recycler: accounts[0] }, fromBlock: 0
         }, function (error, event) {
-            
+
             this.setState({ result: event.returnValues['plasticBottleAddress'], status: event.returnValues['status'] });
             this.setState(prevState => ({ bottlesLogged: [...prevState.bottlesLogged, this.state.result] }));
             this.addRow();
@@ -52,47 +53,47 @@ class recyclerPage extends Component {
         }.bind(this))
             .on('error', console.error);
 
-            // Get all deployed plastic bales SC addresses 
-            trackingContract.events.plasticBaleCompleted({
+        // Get all deployed plastic bales SC addresses 
+        trackingContract.events.plasticBaleCompleted({
+            fromBlock: 0
+        }, function (error, event) {
+            console.log(event);
+            const plasticbaleAddr = event.returnValues['plasticbale'];
+            console.log(plasticbaleAddr);
+            const plasticBaleSC = plasticBaleContract(plasticbaleAddr);
+
+            // Fetch recycler rewards 
+            plasticBaleSC.events.recyclerRewarded({
+                filter: { recycler: accounts[0] },
                 fromBlock: 0
             }, function (error, event) {
                 console.log(event);
-                const plasticbaleAddr = event.returnValues['plasticbale'];
-                console.log(plasticbaleAddr);
-                const plasticBaleSC = plasticBaleContract(plasticbaleAddr);
 
-                // Fetch recycler rewards 
-                plasticBaleSC.events.recyclerRewarded({
-                    filter: { recycler: accounts[0] },
-                    fromBlock: 0
-                }, function (error, event) {
-                    console.log(event);
+                this.setState({
+                    rewards: this.state.rewards + event.returnValues['etherReward'],
+                });
 
-                    this.setState({
-                        rewards: this.state.rewards + event.returnValues['etherReward'],
-                    });
-
-                }.bind(this))
-                    .on('error', console.error);
-
-                    //Update status purchases
-                    plasticBaleSC.events.updateStatusBuyer({
-                        filter: { plasticBottleAddress: this.state.bottlesLogged },
-                        fromBlock: 0
-                    }, function (error, event) {
-                        let index = this.state.bottlesLogged.indexOf(event.returnValues['plasticBottleAddress']);
-                        this.updateRow(index, event.returnValues['status']);
-
-                        }.bind(this))
-                        .on('error', console.error);
-
-    
             }.bind(this))
                 .on('error', console.error);
-        
-    
-        };
-    
+
+            //Update status purchases
+            plasticBaleSC.events.updateStatusBuyer({
+                filter: { plasticBottleAddress: this.state.bottlesLogged },
+                fromBlock: 0
+            }, function (error, event) {
+                let index = this.state.bottlesLogged.indexOf(event.returnValues['plasticBottleAddress']);
+                this.updateRow(index, event.returnValues['status']);
+
+            }.bind(this))
+                .on('error', console.error);
+
+
+        }.bind(this))
+            .on('error', console.error);
+
+
+    };
+
 
     // QR reader functions 
     handleScan = async (data) => {
@@ -150,10 +151,12 @@ class recyclerPage extends Component {
         this.setState({ rows });
     }
 
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
 
     render() {
 
-        const { qr, rows } = this.state
+        const { qr, rows, activeItem } = this.state
 
         return (
             <Layout>
@@ -163,67 +166,108 @@ class recyclerPage extends Component {
                         href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css"
                     />
 
-                    <Grid>
-                        <Grid.Row centered>
-                            <Grid.Column textAlign="center">
-                                <div className="Scanner" style={{ 'width': '25%', 'margin-left': 'auto', 'margin-right': 'auto' }}>
-                                    <h2>Dispose a Plastic Bottle  </h2>
-                                    <Button className="QrReader" onClick={this.onScan} > Scan QR Code</Button>
-                                    <br/>
-                                    <div> {this.state.qr === true ? (
-                                    <QRReader
-                                        delay={300}
-                                        onError={this.handleError}
-                                        onScan={this.handleScan}
-                                        style={{ width: "60%" }}
-                                    />
-                                    )
-                                        : ''} </div>
+                    <Menu text>
+                        <Menu.Item
+                            name='scan'
+                            active={activeItem === 'about'}
+                            onClick={this.handleItemClick}
+                        >
+                            Scan
+                </Menu.Item>
 
-                                </div>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
+                        <Menu.Item
+                            name='history'
+                            active={activeItem === 'features'}
+                            onClick={this.handleItemClick}
+                        >
+                            History
+                </Menu.Item>
+
+                        <Menu.Item
+                            name='rewards'
+                            active={activeItem === 'stats'}
+                            onClick={this.handleItemClick}
+                        >
+                            Rewards
+                </Menu.Item>
+                    </Menu>
 
 
-                    <br />
 
-                    <h3 style={{textAlign:'center'}}>Bottle history:</h3>
+                    {(activeItem === 'scan') && (
+                        <div>
+                            <Grid>
+                                <Grid.Row centered>
+                                    <Grid.Column textAlign="center">
+                                        <div className="Scanner" style={{ 'width': '25%', 'margin-left': 'auto', 'margin-right': 'auto' }}>
+                                            <h2>Dispose a Plastic Bottle  </h2>
+                                            <Button className="QrReader" onClick={this.onScan} > Scan QR Code</Button>
+                                            <br />
+                                            <div> {this.state.qr === true ? (
+                                                <QRReader
+                                                    delay={300}
+                                                    onError={this.handleError}
+                                                    onScan={this.handleScan}
+                                                    style={{ width: "60%" }}
+                                                />
+                                            )
+                                                : ''} </div>
 
-                    <div className='BottleTable' style={{ 'width': '90%', 'margin-left':'auto', 'margin-right':'auto'}}>
-                        <Table unstackable size='small' fixed singleLine>
-                            <Table.Header>
-                                <Table.Row>
-                                    <Table.HeaderCell width={16}>Plasitc Bottle Address</Table.HeaderCell>
-                                    <Table.HeaderCell width={6}>Status</Table.HeaderCell>
-                                </Table.Row>
-                            </Table.Header>
-                            <Table.Body>
-                                {this.state.rows.map(bottle => (
-                                    <Link route={`/track/${bottle.addr}`}>
-                                        <Table.Row id={this.state.rows.length} key={this.state.rows.lengt}>
-                                            <Table.Cell selectable style={{'overflow': 'hidden'}}>
-                                                <a>
-                                                    {bottle.addr}
-                                                </a>
-                                            </Table.Cell>
-                                            <Table.Cell>{bottle.status}</Table.Cell>
+                                        </div>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+
+                        </div>
+                    )}
+
+
+                    {(activeItem === 'history') && (
+                        <div>
+                            <h3 style={{ textAlign: 'center' }}>Bottle history:</h3>
+
+                            <div className='BottleTable' style={{ 'width': '90%', 'margin-left': 'auto', 'margin-right': 'auto' }}>
+                                <Table unstackable size='small' fixed singleLine>
+                                    <Table.Header>
+                                        <Table.Row>
+                                            <Table.HeaderCell width={16}>Plasitc Bottle Address</Table.HeaderCell>
+                                            <Table.HeaderCell width={6}>Status</Table.HeaderCell>
                                         </Table.Row>
-                                    </Link>
-                                ))}
-                            </Table.Body>
+                                    </Table.Header>
+                                    <Table.Body>
+                                        {this.state.rows.map(bottle => (
+                                            <Link route={`/track/${bottle.addr}`}>
+                                                <Table.Row id={this.state.rows.length} key={this.state.rows.lengt}>
+                                                    <Table.Cell selectable style={{ 'overflow': 'hidden' }}>
+                                                        <a>
+                                                            {bottle.addr}
+                                                        </a>
+                                                    </Table.Cell>
+                                                    <Table.Cell>{bottle.status}</Table.Cell>
+                                                </Table.Row>
+                                            </Link>
+                                        ))}
+                                    </Table.Body>
 
-                        </Table>
-                    </div>
+                                </Table>
+                            </div>
+
+                        </div>
+                    )}
+
+
+
+                    {(activeItem === 'rewards') && (
+                        <div>
+                            <h3 style={{ textAlign: 'center' }}>Total rewards:</h3>
+
+                            <Card header='Rewards' description={this.state.rewards} meta='ETH' centered='true' />
+
+                        </div>
+                    )}
+
 
                 </div>
-
-                <h3 style={{textAlign:'center'}}>Total rewards:</h3>
-
-                <Card header='Rewards' description={this.state.rewards} meta='ETH' centered='true' />
-
-                <br />
-
             </Layout>
 
         );
@@ -233,4 +277,4 @@ class recyclerPage extends Component {
 
 //At the end of each page, a component is expected to be returned 
 // If not, Next.js throws an error 
-export default recyclerPage; 
+export default recyclerPage;
