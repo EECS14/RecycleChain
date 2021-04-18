fc53367e942f7f0f3101343432a3bb9c4ca84996
EECS14 <100044615@ku.ac.ae>
2021-04-18 15:14:35 +0400

    Update tracking.js

--- ethereum/tracking.js
+++ ethereum/tracking.js
@@ -1,14 +1,13 @@
 /*
 purpose: To access the contract (register.sol) instance from inside the application without repeating these steps 
-Etherscan: https://ropsten.etherscan.io/tx/0x4aafa039544175312e464969c394bc08125d6baf08ca37b7c6f0f225c4b036a7
-https://ropsten.etherscan.io/tx/0xc97103226f11e65034d14dc1637ad249d9835a618a0ef60da322a2ce7689c0f3
+Etherscan: https://ropsten.etherscan.io/tx/0x00cd3558d090c045da88cb8a5e0460a5882c3688614223dd143b34258e3bd625
 */
 import web3 from './web3'; 
 import Tracking from './build/Tracking.json'; 
 
 const instance = new web3.eth.Contract(
     Tracking.abi,
-     '0xdf64abab64a4520821ad6586c897ddd9aefd0081'
+     '0x5bcFbb0e3CA2502577A8C1D32D2567f4ED176D12'
      // Address of the tracking.sol contract in ropsten network
 );
 
