ca1a2f0852fcc7053a7762e832714bb40d35f965
EECS14 <100044615@ku.ac.ae>
2021-05-01 13:54:00 +0400

    Update web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -12,10 +12,19 @@ let web3;
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   // We are in the browser and metamask is running.
   //Connect metamask to the webapp 
+  console.log('Metamask is enabled');
   window.ethereum.enable(); 
   web3 = new Web3(window.web3.currentProvider);
+
+  if (web3.currentProvider.isMetaMask === true) {
+    console.log('MetaMask is active');
+  }
+
 } else {
   // We are on the server *OR* the user is not running metamask
+  console.log('Metamask is not enabled');
+  // Comment and uncomment this line for DApp in mobile view to work
+  window.ethereum.enable(); 
   const provider = new Web3.providers.HttpProvider( 'https://ropsten.infura.io/v3/a5dc022ed8df4754b509e1f289d10b05');
   web3 = new Web3(provider);
 }  
